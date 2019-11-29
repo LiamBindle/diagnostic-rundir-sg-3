3e5de333648555717d3baf384e6d68149b74d8f3
Liam Bindle <liambindle@gmail.com>
2019-11-29 12:57:46 -0400

    initial version

---
+++ clean-rundir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -f /n/home/gchp_RnPbBe/OutputDir/*.nc4
+rm -f trac_avg.*
+rm -f tracerinfo.dat
+rm -f diaginfo.dat
+rm -f cap_restart
+rm -f gcchem*
+rm -f *.rcx
+rm -f *~
+rm -f gchp.log
+rm -f HEMCO.log
+rm -f PET*.log
+rm -f multirun.log
+rm -f logfile.000000.out
+rm -f slurm-*
+rm -f 1
+rm -f EGRESS
